923cb7c06db3cf0c659c1f6d3cffe0060944b8cb
gjb711 <gjb711@qq.com>
2026-09-14 10:13:12 +0800

    Fix packaging: declare cryptography dependency, require Python>=3.9; install deps + smoke-test in workflow

--- pyora/__init__.py
+++ pyora/__init__.py
@@ -24,7 +24,7 @@ from .configurations import parse_config, ConnectionConfig
 from .network.oracle_error import OracleError
 from .connection import Connection
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 __all__ = ["connect", "Connection", "OracleError", "parse_config", "Cursor", "ConnectionWrapper"]
 
